0bb2c1c6c5db6d5a74ffd2a75997a0537b5c3143
Jackson Bates <jkbates222@gmail.com>
2018-09-11 00:44:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!";
+}
+function happyHolidaysTo(string) {
+  return `Happy holidays\, ${string}!`;
+}
+function happyHolidayTo(holiday, string) {
+  return `Happy ${holiday}\, ${string}!`;
+}
